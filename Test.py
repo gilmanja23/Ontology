08de2c90b0e22e5b119a863fa2e1c6ff48861ea3
gilmanja23 <87087958+gilmanja23@users.noreply.github.com>
2021-07-09 16:04:18 -0400

    Add files via upload
    Test for uploading to github repository

---
+++ Test.py
@@ -0,0 +1,22 @@
+import math
+
+
+class Test:
+    def __init__(self):
+        self.sum = 0
+
+    @staticmethod
+    def add_two_numbers(number_one, number_two):
+        return number_one + number_two
+
+    @staticmethod
+    def multiply_two_numbers(number_one, number_two):
+        return number_one * number_two
+
+    @staticmethod
+    def subtract_two_numbers(number_one, number_two):
+        return number_one - number_two
+
+    @staticmethod
+    def divide_two_numbers(number_one, number_two):
+        return number_one / number_two
